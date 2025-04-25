562f6287425792d874850792a5010568303dc1fd
LoiNguyen0815 <thanhloi1508@gmail.com>
2025-04-25 23:08:24 +0700

    [US-01] click button

---
+++ tests/element/sigle-element/button.spec.ts
@@ -0,0 +1,11 @@
+import test, {Page} from "@playwright/test";
+
+test('Test button', async ({page}) => {
+    await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/button');
+    await clickButton(page, "Default");
+})
+
+async function clickButton(page: Page, label : string) {
+    let xpath = `//button[.//text()[normalize-space()='${label}']]`;
+    await page.click(xpath);
+}
