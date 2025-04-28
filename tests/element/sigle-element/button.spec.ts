b6ce049a71b81f11e0143d73bc3f44d3f1ee6e68
LoiNguyen0815 <thanhloi1508@gmail.com>
2025-04-28 21:24:04 +0700

    [US-03] Implement click button

--- tests/element/sigle-element/button.spec.ts
+++ tests/element/sigle-element/button.spec.ts
@@ -1,11 +1,39 @@
-import test, {Page} from "@playwright/test";
+import test, {expect, Page} from "@playwright/test";
 
-test('Test button', async ({page}) => {
+test.beforeEach('Test Before', async ({page}) => {
     await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/button');
-    await clickButton(page, "Default");
+})
+
+// using Parameterize tests
+let buttons = ['Origin button', 'Default', 'Primary']
+for(let button of buttons){
+    test(`Test Button: ${button} `, async ({page}) => {
+        await clickButton(page, button);
+        await expect(page.getByText(`Button ${button} was clicked!`)).toBeVisible();
+    })
+}
+
+test('Test Role Button', async ({page}) => {
+    await clickRoleButton(page, "Div button");
+    await expect(page.getByText("Button Div button was clicked!")).toBeVisible();
+})
+
+test('Test Input Button', async ({page}) => {
+    await clickInputButton(page, "Input button");
+    await expect(page.getByText("Button Input button was clicked!")).toBeVisible();
 })
 
 async function clickButton(page: Page, label : string) {
     let xpath = `//button[.//text()[normalize-space()='${label}']]`;
+    await page.click(xpath);
+}
+
+async function clickRoleButton(page: Page, label : string) {
+    let xpath = `//*[@role='button' and .//text()[normalize-space()='${label}']]`;
+    await page.click(xpath);
+}
+
+async function clickInputButton(page: Page, label : string) {
+    let xpath = `//input[@type='button' and @value[normalize-space()='${label}']]`;
     await page.click(xpath);
 }
